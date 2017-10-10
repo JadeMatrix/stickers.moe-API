0b8b309621d5058c144724185b93be84a19db11b
JadeMatrix <jadematrix.art@gmail.com>
2017-10-10 02:09:40 -0400

    Added basic skeleton of server program

---
+++ src/server/main.cpp
@@ -0,0 +1,61 @@
+#include <fstream>
+#include <iostream>
+
+#include "../common/formatting.hpp"
+#include "../common/json.hpp"
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+int main( int argc, char* argv[] )
+{
+    if( argc < 2 )
+    {
+        ff::writeln(
+            std::cerr,
+            "usage: ",
+            argv[ 0 ],
+            " config.json"
+        );
+        return 1;
+    }
+    
+    try
+    {
+        nlj::json config;
+        
+        {
+            std::ifstream config_file( argv[ 1 ] );
+            
+            if( config_file.is_open() )
+                config_file >> config;
+            else
+            {
+                ff::writeln(
+                    std::cerr,
+                    "could not open config file ",
+                    argv[ 1 ]
+                );
+                return 2;
+            }
+        }
+        
+        // DEBUG:
+        ff::writeln(
+            std::cout,
+            config.dump()
+        );
+    }
+    catch( const std::exception &e )
+    {
+        ff::writeln(
+            std::cerr,
+            "uncaught exception in main(): ",
+            e.what()
+        );
+        return -1;
+    }
+    
+    return 0;
+}
