60df2ab8e6bb4242186ea8349944625c38f9bccc
JadeMatrix <jadematrix.art@gmail.com>
2017-10-21 17:11:34 -0400

    Added missing "common/hashing.hpp"

---
+++ src/common/hashing.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef STICKERS_MOE_COMMON_HASHING_HPP
+#define STICKERS_MOE_COMMON_HASHING_HPP
+
+
+#include <string>
+
+
+namespace stickers
+{
+    // class sha256
+    // {
+    // protected:
+    //     unsigned char bytes[ 8 ];
+    // public:
+    //     // sha256( const std::string& );
+    //     sha256( const char* );
+    //     to_hex();
+    // };
+}
+
+
+#endif
