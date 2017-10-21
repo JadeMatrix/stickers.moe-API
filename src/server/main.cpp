759d755d2c76431ac3c72c4544808455aa415c08
JadeMatrix <jadematrix.art@gmail.com>
2017-10-21 14:50:07 -0400

    Modified the server to test user loading

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,12 @@
 #include <fstream>
 #include <iostream>
 
+#include "../common/config.hpp"
 #include "../common/formatting.hpp"
 #include "../common/json.hpp"
+
+// DEVEL:
+#include "../common/user.hpp"
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -23,9 +27,8 @@ int main( int argc, char* argv[] )
     
     try
     {
-        nlj::json config;
-        
         {
+            nlj::json config;
             std::ifstream config_file( argv[ 1 ] );
             
             if( config_file.is_open() )
@@ -39,12 +42,44 @@ int main( int argc, char* argv[] )
                 );
                 return 2;
             }
+            
+            stickers::set_config( config );
         }
         
-        // DEBUG:
+        // // DEBUG:
         ff::writeln(
             std::cout,
-            config.dump()
+            "config: ",
+            stickers::config().dump()
+        );
+        
+        // DEVEL:
+        stickers::user_info info = stickers::load_user( stickers::BIGID_MIN );
+        nlj::json user = {
+            { "user_id", ( long long )( stickers::BIGID_MIN ) },
+            { "password", {
+                // { "type",   info.password.type   },
+                { "hash",   info.password.hash   },
+                { "salt",   info.password.salt   },
+                { "factor", info.password.factor },
+            } },
+            // { "created",  },
+            // { "revised",  },
+            { "display_name", info.display_name },
+            { "real_name", info.real_name },
+            // { "avatar_hash", info.avatar_hash },
+            { "email", info.email }
+        };
+        if( info.real_name == "" )
+            user[ "real_name" ] = nullptr;
+        // if( info.avatar_hash == "" )
+        //     user[ "avatar_hash" ] = nullptr;
+        user[ "user_id" ] = ( long long )( stickers::BIGID_MIN );
+        
+        ff::writeln(
+            std::cout,
+            "user: ",
+            user.dump()
         );
     }
     catch( const std::exception &e )
