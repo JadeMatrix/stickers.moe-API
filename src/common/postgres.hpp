96d9bd68132dc477870ebf5174dffb69a6e266d8
JadeMatrix <jadematrix.art@gmail.com>
2017-10-21 14:48:59 -0400

    Added some utils for PostgreSQL awaiting support in libpqxx, including naïve composite parsing & un-quoting/escaping

--- src/common/postgres.hpp
+++ src/common/postgres.hpp
@@ -3,9 +3,31 @@
 #define STICKERS_MOE_COMMON_POSTGRES_HPP
 
 
+#include "exception.hpp"
+
 #include <pqxx/pqxx>
 
+#include <string>
+#include <vector>
+
+
 #define PSQL( ... ) #__VA_ARGS__
+
+namespace stickers
+{
+    // Utility to split composite types until libpqxx supports that
+    
+    std::vector< std::string > split_pg_field( const pqxx::field& );
+    
+    class non_composite_field : public exception
+    {
+    protected:
+        std::string message;
+    public:
+        non_composite_field( const pqxx::field& );
+        virtual const char* what() const noexcept;
+    };
+}
 
 
 #endif
