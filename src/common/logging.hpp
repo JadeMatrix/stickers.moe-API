fad6ecc2b9489d46f2bc88ef559f7d96cd80bc8d
JadeMatrix <jadematrix.art@gmail.com>
2017-11-28 15:19:29 -0500

    Added macro-based logging utilities

---
+++ src/common/logging.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef STICKERS_MOE_COMMON_LOGGING_HPP
+#define STICKERS_MOE_COMMON_LOGGING_HPP
+
+
+#include "config.hpp"
+#include "formatting.hpp"
+
+#include <ctime>
+#include <iostream>
+
+
+#define STICKERS_LOG( LEVEL, ... ) \
+    if( stickers::log_level() >= stickers::LEVEL ) \
+    { \
+        char tb[ 256 ]; \
+        auto t = std::time( nullptr ); \
+        std::strftime( tb, sizeof( tb ), "%F %T%z", std::localtime( &t ) ); \
+        ff::writeln( \
+            ( stickers::LEVEL == stickers::ERRORS ? std::cerr : std::cout ), \
+            "[", #LEVEL, "]", \
+            "[", tb, "]", \
+            stickers::log_level() >= stickers::DEBUG ? ( \
+                std::string( "[" ) \
+                + __FILE__ \
+                + ":" \
+                + std::to_string( __LINE__ ) \
+                + "] " \
+            ) : " ", \
+            __VA_ARGS__ \
+        ); \
+    }
+
+
+#endif
