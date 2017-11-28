5c431a7f477fc175e9a2f12936352262b9036037
JadeMatrix <jadematrix.art@gmail.com>
2017-11-28 01:37:42 -0500

    Added in basic server functionality using SHOW (which is header-only so no changes to CMakeLists.txt)

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 
+#include <show.hpp>
 #include "../common/config.hpp"
 #include "../common/formatting.hpp"
 #include "../common/json.hpp"
@@ -46,48 +47,95 @@ int main( int argc, char* argv[] )
             stickers::set_config( config );
         }
         
-        // // DEBUG:
+        // DEBUG:
         ff::writeln(
             std::cout,
             "config: ",
             stickers::config().dump()
         );
         
-        // DEVEL:
-        stickers::user_info info = stickers::load_user( stickers::BIGID_MIN );
-        nlj::json user = {
-            { "user_id", ( long long )( stickers::BIGID_MIN ) },
-            { "password", {
-                // { "type",   info.password.type   },
-                { "hash",   info.password.hash   },
-                { "salt",   info.password.salt   },
-                { "factor", info.password.factor },
-            } },
-            { "created", info.created },
-            { "revised", info.revised },
-            { "display_name", info.display_name },
-            { "real_name", info.real_name },
-            // { "avatar_hash", info.avatar_hash },
-            { "email", info.email }
-        };
-        switch( info.password.type )
-        {
-        case stickers::BCRYPT:
-            user[ "password" ][ "type" ] = "bcrypt";
-        default:
-            user[ "password" ][ "type" ] = nullptr;
-        }
-        if( info.real_name == "" )
-            user[ "real_name" ] = nullptr;
-        // if( info.avatar_hash == "" )
-        //     user[ "avatar_hash" ] = nullptr;
-        user[ "user_id" ] = ( long long )( stickers::BIGID_MIN );
-        
-        ff::writeln(
-            std::cout,
-            "user: ",
-            user.dump()
+        show::server server(
+            stickers::config()[ "server" ][ "host"    ],
+            stickers::config()[ "server" ][ "port"    ],
+            stickers::config()[ "server" ][ "timeout" ]
         );
+        
+        // DEVEL:
+        while( true )
+            try
+            {
+                show::connection connection( server.serve() );
+                
+                while( true )
+                    try
+                    {
+                        show::request request( connection );
+                        
+                        if( !request.unknown_content_length )
+                            while( !request.eof() ) request.sbumpc();
+                        
+                        stickers::user_info info = stickers::load_user(
+                            stickers::BIGID_MIN
+                        );
+                        nlj::json user = {
+                            { "user_id", ( long long )( stickers::BIGID_MIN ) },
+                            { "password", {
+                                // { "type",   info.password.type   },
+                                { "hash",   info.password.hash   },
+                                { "salt",   info.password.salt   },
+                                { "factor", info.password.factor },
+                            } },
+                            { "created", info.created },
+                            { "revised", info.revised },
+                            { "display_name", info.display_name },
+                            { "real_name", info.real_name },
+                            // { "avatar_hash", info.avatar_hash },
+                            { "email", info.email }
+                        };
+                        switch( info.password.type )
+                        {
+                        case stickers::BCRYPT:
+                            user[ "password" ][ "type" ] = "bcrypt";
+                        default:
+                            user[ "password" ][ "type" ] = nullptr;
+                        }
+                        if( info.real_name == "" )
+                            user[ "real_name" ] = nullptr;
+                        // if( info.avatar_hash == "" )
+                        //     user[ "avatar_hash" ] = nullptr;
+                        user[ "user_id" ] = ( long long )( stickers::BIGID_MIN );
+                        
+                        std::string user_json = user.dump();
+                        
+                        show::response response(
+                            request,
+                            show::HTTP_1_0,
+                            { 200, "OK" },
+                            {
+                                { "Server", {
+                                    show::version.name
+                                    + " v"
+                                    + show::version.string
+                                } },
+                                { "Content-Type", { "application/json" } },
+                                { "Content-Length", {
+                                    std::to_string( user_json.size() )
+                                } }
+                            }
+                        );
+                        
+                        response.sputn( user_json.c_str(), user_json.size() );
+                    }
+                    catch( show::client_disconnected& cd )
+                    {
+                        break;
+                    }
+                    catch( show::connection_timeout& ct )
+                    {
+                        break;
+                    }
+            }
+            catch( show::connection_timeout& ct ) {}
     }
     catch( const std::exception &e )
     {
