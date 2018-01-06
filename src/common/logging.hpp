9d44e55addf8b972e6b6e2ab0bd1758c1fec4fb8
JadeMatrix <jadematrix.art@gmail.com>
2018-01-05 20:42:06 -0500

    Replaced logging macro with a template function & macro combo

--- src/common/logging.hpp
+++ src/common/logging.hpp
@@ -12,23 +12,48 @@
 #include <sstream>
 
 
-#define STICKERS_LOG( LEVEL, ... ) \
-    if( stickers::log_level() >= stickers::LEVEL ) \
-    { \
-        ff::writeln( \
-            ( stickers::LEVEL == stickers::ERROR ? std::cerr : std::cout ), \
-            "[", #LEVEL, "]", \
-            "[", stickers::to_iso8601_str( stickers::current_timestamp() ), "]", \
-            stickers::log_level() >= stickers::DEBUG ? ( \
-                std::string( "[" ) \
-                + __FILE__ \
-                + ":" \
-                + std::to_string( __LINE__ ) \
-                + "] " \
-            ) : " ", \
-            __VA_ARGS__ \
-        ); \
+namespace stickers
+{
+    template< typename... Args > void log(
+        log_level_type level,
+        std::string file_name,
+        long long file_line,
+        Args... args
+    )
+    {
+        if( stickers::log_level() >= level )
+        {
+            auto t = std::time( nullptr );
+            std::stringstream time_string;
+            time_string << std::put_time(
+                std::localtime( &t ),
+                "%F %T%z"
+            );
+            
+            ff::writeln(
+                ( level == stickers::ERROR ? std::cerr : std::cout ),
+                "[", ( long )level, "]",
+                "[", time_string.str(), "]",
+                stickers::log_level() >= stickers::DEBUG ? (
+                    std::string( "[" )
+                    + file_name
+                    + ":"
+                    + std::to_string( file_line )
+                    + "] "
+                ) : "",
+                args...
+            );
+        }
     }
+}
+
+
+#define STICKERS_LOG( LEVEL, ... ) stickers::log( \
+    stickers::LEVEL, \
+    __FILE__, \
+    __LINE__, \
+    __VA_ARGS__ \
+)
 
 
 #endif
