c8d2096345d336a4e75d190d835f46cf10719dca
JadeMatrix <jadematrix.art@gmail.com>
2018-03-28 02:46:44 -0400

    Implemented basic login — no auth tokens returned, only a success message

--- src/handlers/auth.cpp
+++ src/handlers/auth.cpp
@@ -3,6 +3,10 @@
 
 #include "handlers.hpp"
 
+#include "../api/user.hpp"
+#include "../server/parse.hpp"
+#include "../server/server.hpp"
+
 
 namespace stickers
 {
@@ -13,6 +17,60 @@ namespace stickers
     
     void handlers::login( show::request& request )
     {
-        throw handler_exit{ { 500, "Not Implemented" }, "Not implemented" };
+        auto content = parse_request_content( request );
+        
+        for( const auto field : {
+            "email",
+            "password"
+        } )
+            if( content.find( field ) == content.end() )
+                throw handler_exit{
+                    { 400, "Bad Request" },
+                    "missing required field \""
+                    + static_cast< std::string >( field )
+                    + "\""
+                };
+            else if( !content[ field ].is_string() )
+                throw handler_exit{
+                    { 400, "Bad Request" },
+                    "required field \""
+                    + static_cast< std::string >( field )
+                    + "\" must be a string"
+                };
+        
+        try
+        {
+            auto user = load_user_by_email(
+                content[ "email" ].get< std::string >()
+            );
+            
+            if(
+                user.info.password
+                == content[ "password" ].get< std::string >()
+            )
+            {
+                show::response response{
+                    request.connection(),
+                    show::HTTP_1_1,
+                    { 200, "OK" },
+                    {
+                        server_header,
+                        { "Content-Length", { "0" } },
+                        { "Location", {
+                            "/user/" + static_cast< std::string >( user.id )
+                        } }
+                    }
+                };
+                
+                return;
+            }
+        }
+        catch( const no_such_user& e )
+        {}
+        
+        throw handler_exit{
+            { 401, "Unauthorized" },
+            "check email & password"
+        };
     }
 }
